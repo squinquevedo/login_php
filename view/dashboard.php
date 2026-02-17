5c7d19958b84eab4374e80f384b75e9704b8ea33
squinquevedo <squinquevedo@gmail.com>
2026-02-17 18:32:07 -0500

    diapositiva 20

---
+++ view/dashboard.php
@@ -0,0 +1,14 @@
+<!--view/dashboard.php-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dashboard</title>
+</head>
+<body>
+    <h1>bienvenido al dashboard</h1>
+    <p>has iniciado sesion exitosamente</p>
+    <a href="index.php?action=logout">cerrar sesion</a>
+</body>
+</html>
